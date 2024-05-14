86ba93d7a55fc4f2f37b3fc53208137a39954cf2
tranvancanhtozan <85920372+tranvancanhtozan@users.noreply.github.com>
2024-05-14 11:07:15 +0900

    user context

---
+++ MauiUI/Services/UserContextService.cs
@@ -0,0 +1,21 @@
+﻿using MauiUI.Models;
+
+namespace MauiUI.Services
+{
+    public interface IUserContextService
+    {
+        Task<UserTable> GetUserAsync();
+    }
+
+    public class UserContextService : IUserContextService
+    {
+        public async Task<UserTable> GetUserAsync()
+        {
+            // get user from Sql Lite
+            var user =  new UserTable();
+
+            await Task.CompletedTask;
+            return user;
+        }
+    }
+}
