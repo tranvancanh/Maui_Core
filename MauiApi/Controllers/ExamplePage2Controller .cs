7269af473e37de963d7e4c59f85a4ecd95e40a65
tranvancanhtozan <85920372+tranvancanhtozan@users.noreply.github.com>
2024-05-14 11:41:02 +0900

    add controler

---
+++ MauiApi/Controllers/ExamplePage2Controller .cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MauiApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamplePage2Controller : ControllerBase
+    {
+    }
+}
---
+++ MauiApi/Controllers/ExamplePage3Controller.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MauiApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ExamplePage3Controller : ControllerBase
+    {
+    }
+}
